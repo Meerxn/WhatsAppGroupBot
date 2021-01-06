f3cdfa5621fdb4b0b25ddcc1d4be9e61a0208066
Fardeen Meeran <61164969+Meerxn@users.noreply.github.com>
2021-01-06 12:25:42 +0400

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const qrcode = require('qrcode-terminal');
 const fs = require('fs');
 
 const { Client } = require('whatsapp-web.js');
+const { homedir } = require('os');
 //const client = new Client();
 const SESSION_FILE_PATH = './session.json';
 
@@ -40,6 +41,127 @@ client.initialize();
 
 
 var groupType = "";
+
+client.on('message' , async(msg) =>{
+ 
+ if (msg.body === "create_label"){
+    var count  = 0
+    
+
+    
+
+    chat = await msg.getChat();
+    var nm = chat.name;
+    counter = 0  
+    
+    userList  = [];
+    
+    for (let usr of chat.participants){
+        console.log()
+        
+
+        const contact = await client.getContactById(usr.id._serialized);
+        console.log(contact);
+        userList.push(contact.pushname);
+        console.log(userList);
+        
+
+    }
+    if (counter === 0 ){
+        ho(msg,chat)
+    }
+    
+    
+    
+
+    chat.sendMessage("Please state the name of the label");
+    client.on('message' , async(message) =>{
+        chat = await message.getChat();
+        if (chat.name ===  nm && count === 0 ) {
+
+        chat.sendMessage("Please choose the users that you want for " + message.body + "by typing 1 if you want this user and 0 if you dont. Suppose There are 2 out of 3 users you want to pick type 011 if you want user 2 and 3 but not user 1");
+        chat.sendMessage("Here is the assorted user list");
+        console.log(userList);
+        var users  = "" ;
+        for( var i = 0 ; i < userList.length ; i++){
+            users = users + " " +  userList[i];
+        }
+        chat.sendMessage(users)
+        
+        //chat.sendMessage(userList);
+        client.on('message' , async(members) =>{
+            
+            newLabel = [];
+            for (var i = 0 ; i < members.body.length ; i++){
+
+                if (members.body.charAt(i) === "1"){
+                    newLabel.push(userList[i]);
+
+
+
+
+                }
+            }
+
+            //chat.sendMessage("test tags for now");
+            let text = "";
+            let mentions = [];
+    
+            for(let participant of chat.participants) {
+                const contact = await client.getContactById(participant.id._serialized);
+                for (var j = 0 ; j < newLabel.length ; j++){
+                    
+                    if (newLabel[j] === contact.name){
+                        mentions.push(contact);
+                    text += `@${participant.id.user} `;
+
+                    }
+                }
+                
+            }
+    
+            chat.sendMessage(text, { mentions });
+            count = count + 1
+            return;
+
+            
+            
+    
+    
+            
+    
+        
+        });
+    }
+
+
+
+        
+    });
+}
+
+});
+
+function ho(m,c){
+    c.sendMessage("blah");
+    client.on('message', async(trial) =>{
+        if (trial.body === "kkk"){
+            c.sendMessage("blahhhh")
+            return;
+
+        }
+    });
+    return;
+}
+        
+
+
+  
+
+
+
+
+
 //EVERYONE TAG
 client.on('message', async (msg) => {
     const chat = await msg.getChat();
